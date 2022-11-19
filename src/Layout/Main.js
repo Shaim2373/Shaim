c40d27db305f8d87d059c532af2741348513226e
Shaim2373 <shaimrahman2368@gmail.com>
2022-11-19 21:04:17 +0600

    some problem solve

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import Footer from '../Component/Footer/Footer';
 import Header from '../Component/Header/Header';
 
+
 const Main = () => {
     return (
         <div>
